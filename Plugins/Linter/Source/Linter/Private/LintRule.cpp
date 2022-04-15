5fddd8ba64d709ec844ad409d9c4927bef7b5858
GlebShikovec <gleb.shikovec@gmail.com>
2022-04-15 11:55:06 +0300

    5.0.0-release pathups

--- Plugins/Linter/Source/Linter/Private/LintRule.cpp
+++ Plugins/Linter/Source/Linter/Private/LintRule.cpp
@@ -61,11 +61,11 @@ FName ULintRule::GetRuleBasedObjectVariantName_Implementation(UObject* ObjectToL
 		UMaterialInterface* MI = Cast<UMaterialInterface>(ObjectToLint);
 		if (MI != nullptr)
 		{
-#if ENGINE_MINOR_VERSION >= 25
+//#if ENGINE_MINOR_VERSION >= 25
 			TMicRecursionGuard RecursionGuard;
-#else
-			UMaterialInterface::TMicRecursionGuard RecursionGuard;
-#endif
+//#else
+//			UMaterialInterface::TMicRecursionGuard RecursionGuard;
+//#endif
 			const UMaterial* Material = MI->GetMaterial_Concurrent(RecursionGuard);
 			if (Material != nullptr)
 			{
