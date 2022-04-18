830203d10edc19eb49941c684d3311a8107e6d43
empty2fill <empty2fill@gmail.com>
2022-04-18 16:25:34 +0900

    Sort Class Naming Conventions on PreSave
    (cherry picked from commit 307e7505afd0905373ea9680b421a54a36f4a19c)

--- Plugins/Linter/Source/Linter/Public/LinterNamingConvention.h
+++ Plugins/Linter/Source/Linter/Public/LinterNamingConvention.h
@@ -74,6 +74,8 @@ public:
 	UFUNCTION(Blueprintcallable, Category = "Conventions")
 	void SortConventions();
 
+	virtual void PreSave(const class ITargetPlatform* TargetPlatform) override;
+
 protected:
 
 	
